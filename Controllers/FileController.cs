f3a6ad9102c458ec95e05d2935fb3ad67c9138f6
nouarismail <nouaresmail@gmail.com>
2023-12-09 19:34:10 +0300

    open read stream

--- Controllers/FileController.cs
+++ Controllers/FileController.cs
@@ -25,7 +25,7 @@ namespace MyPdfService.Controllers
         public FileController(AppDbContext dbContext)
         {
             _dbContext = dbContext;
-            
+
         }
 
         [HttpPost("upload")]
@@ -34,42 +34,67 @@ namespace MyPdfService.Controllers
             if (file == null || file.Length == 0)
                 return BadRequest("Invalid file.");
 
-            using (var memoryStream = new MemoryStream())
+            int chunkSize = 8192; // 8 KB
+            List<byte[]> chunkList = new List<byte[]>();
+
+            using (var stream = file.OpenReadStream())
             {
-                await file.CopyToAsync(memoryStream);
-                var fileModel = new FileModel
-                {
-                    FileName = file.FileName,
-                    Content = memoryStream.ToArray()
-                };
+                byte[] buffer = new byte[chunkSize];
+                long totalBytesRead = 0;
 
-
-                _dbContext.Files.Add(fileModel);
-                try
+                while (true)
                 {
-                    await _dbContext.SaveChangesAsync();
-                }
-                catch (System.Exception e)
-                {
+                    int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
 
-                    Console.WriteLine(e.Message);
+                    if (bytesRead == 0)
+                        break;
+
+                    byte[] chunk = new byte[bytesRead];
+                    Array.Copy(buffer, chunk, bytesRead);
+
+                    chunkList.Add(chunk);
+                    totalBytesRead += bytesRead;
                 }
 
-                var retryPolicy = Policy
-                .Handle<Exception>() // Retry on any exception
-                .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
-
-
-                var pdfFileName = $"{fileModel.Id}_output.pdf";
-
-
-                await retryPolicy.ExecuteAsync(async () => await ConvertToPdf(fileModel.Content, pdfFileName));
-
-                fileModel.PdfFileName = pdfFileName;
-                await _dbContext.SaveChangesAsync();
-
-                return Ok(new { DownloadLink = Url.Action("DownloadFile", new { id = fileModel.Id }) });
+                // Additional processing logic after reading the entire file
+                Console.WriteLine($"Total bytes read: {totalBytesRead}");
             }
+
+            // Concatenate all chunks into a single byte array
+            byte[] htmlContent = chunkList.SelectMany(chunk => chunk).ToArray();
+
+            var fileModel = new FileModel
+            {
+                FileName = file.FileName,
+                Content = htmlContent
+            };
+
+
+            _dbContext.Files.Add(fileModel);
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (System.Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+
+            var retryPolicy = Policy
+            .Handle<Exception>() // Retry on any exception
+            .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+
+
+            var pdfFileName = $"{fileModel.Id}_output.pdf";
+
+
+            await retryPolicy.ExecuteAsync(async () => await ConvertToPdf(fileModel.Content, pdfFileName));
+
+            fileModel.PdfFileName = pdfFileName;
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(new { DownloadLink = Url.Action("DownloadFile", new { id = fileModel.Id }) });
         }
 
         [HttpGet("download/{id}")]
@@ -79,8 +104,8 @@ namespace MyPdfService.Controllers
 
             if (fileModel == null)
                 return NotFound();
-            string? directoryPath = string.IsNullOrEmpty(configuration["DirectoryPath"])?"./Outputs/":configuration["DirectoryPath"];
-            var fileBytes = System.IO.File.ReadAllBytes(directoryPath+fileModel.PdfFileName);
+            string? directoryPath = string.IsNullOrEmpty(configuration["DirectoryPath"]) ? "./Outputs/" : configuration["DirectoryPath"];
+            var fileBytes = System.IO.File.ReadAllBytes(directoryPath + fileModel.PdfFileName);
             return File(fileBytes, "application/pdf", fileModel.PdfFileName);
         }
 
@@ -97,7 +122,7 @@ namespace MyPdfService.Controllers
 
             using var browser = await Puppeteer.LaunchAsync(launchOptions);
             using var page = await browser.NewPageAsync();
-            string? directoryPath = string.IsNullOrEmpty(configuration["DirectoryPath"])?"./Outputs/":configuration["DirectoryPath"];
+            string? directoryPath = string.IsNullOrEmpty(configuration["DirectoryPath"]) ? "./Outputs/" : configuration["DirectoryPath"];
             if (Directory.Exists(directoryPath))
             {
                 await page.SetContentAsync(Encoding.UTF8.GetString(htmlContent));
@@ -111,7 +136,7 @@ namespace MyPdfService.Controllers
                 await page.SetContentAsync(Encoding.UTF8.GetString(htmlContent));
                 await page.PdfAsync(directoryPath + pdfFileName);
             }
-            
+
 
         }
     }
